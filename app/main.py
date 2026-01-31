cf05b4d8e7b397f9b0b9825e13b202afd5c62211
Oleg Morgun <raubtierm@gmail.com>
2026-01-31 23:45:03 +0700

    Управление корзиной

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import categories, products, users, reviews
+from app.routers import cart, categories, products, reviews, users
 
 # Создаём приложение FastAPI
 app = FastAPI(
@@ -9,10 +9,11 @@ app = FastAPI(
 )
 
 # Подключаем маршруты категорий и товаров
+app.include_router(cart.router)
 app.include_router(categories.router)
 app.include_router(products.router)
-app.include_router(users.router)
 app.include_router(reviews.router)
+app.include_router(users.router)
 
 
 # Корневой эндпоинт для проверки
--- app/models/products.py
+++ app/models/products.py
@@ -41,7 +41,10 @@ class Product(Base):
 
     category: Mapped["Category"] = relationship(back_populates="products")
     seller: Mapped["User"] = relationship("User", back_populates="products")
-    reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product", cascade="all, delete-orphan")
+    reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product",
+                                                   cascade="all, delete-orphan")
+    cart_items: Mapped[list["CartItem"]] = relationship("CartItem", back_populates="product",
+                                                        cascade="all, delete-orphan")
 
     __table_args__ = (
         Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
